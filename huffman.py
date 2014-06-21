c57f4f442adfa6ed8cd2734b848505a46f330086
Chris <christian_tessnow@yahoo.de>
2014-06-22 01:04:05 +0200

    final commit

--- huffman.py
+++ huffman.py
@@ -1,13 +1,12 @@
 import math
-from binarytree import BinaryTree
 
 
-def int2bin(dec):
+def int2code(dec):
     r = ''
-    while dec > 1:
-        r = str(dec % 2) + r
+    while dec != 1:
+        r += ('0' if ((dec / 2) % math.floor(dec / 2)) < 0.5 else '1')
         dec = math.floor(dec / 2)
-    return str(dec) + r
+    return r[::-1]
 
 
 def getindex(collection, key):
@@ -21,23 +20,20 @@ def getindex(collection, key):
 
 text = input("Key in a text!\n").lower()
 l = []
-ignore = [' ', '.', ',', ';', '!', '?', '"', "'"]
 for c in text:
-    if c not in ignore:
-        x = getindex(l, c)
-        if x != -1:
-            new_tuple = (c, l[x][1] + 1)
-            l.remove((c, l[x][1]))
-            l.append(new_tuple)
-        else:
-            l.append((c, 1))
+    x = getindex(l, c)
+    if x != -1:
+        new_tuple = (c, l[x][1] + 1)
+        l.remove((c, l[x][1]))
+        l.append(new_tuple)
+    else:
+        l.append((c, 1))
 
-sorted_l = sorted(l, key=lambda t: t[1], reverse=True)
-print(sorted_l)
-b = BinaryTree('root', None, None)
-i = 0
-for e in sorted_l:
-    print(e[0]+": "+int2bin(i))
-    b.insert(e[0], int2bin(i))
+l = sorted(l, key=lambda t: t[1], reverse=True)
+code = {}
+i = 2
+for e in l:
+    code[e[0]] = int2code(i)
     i += 1
-print(b)
+for c in text:
+    print(code[c], end=" ")
