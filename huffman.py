d4eedb01a468f127fa7140835753e4bea077ea9a
Chris <christian_tessnow@yahoo.de>
2014-06-21 21:51:16 +0200

    updated huffman.py deleted binarytree.py

--- huffman.py
+++ huffman.py
@@ -1,4 +1,12 @@
-__author__ = 'Christian'
+import math
+
+
+def int2bin(dec):
+    r = ''
+    while dec > 1:
+        r = str(dec % 2) + r
+        dec = math.floor(dec / 2)
+    return str(dec) + r
 
 
 def isin(collection, key):
@@ -12,13 +20,20 @@ def isin(collection, key):
 
 text = input("Key in a text!\n").lower()
 l = []
+ignore = [' ', '.', ',', ';', '!', '?', '"', "'"]
 for c in text:
-    if c != " ":
+    if c not in ignore:
         x = isin(l, c)
         if x != -1:
-            t = (c, l[x][1]+1)
+            t = (c, l[x][1] + 1)
             l.remove((c, l[x][1]))
             l.append(t)
         else:
             l.append((c, 1))
-print(l)
+
+sorted_l = sorted(l, key=lambda e: e[1], reverse=True)
+print(sorted_l)
+i = 0
+for e in sorted_l:
+    print(e[0]+": "+int2bin(i))
+    i += 1
